488f7c57a4128f35f4bfdb7d8acf3f17723581ba
Jeremy <jeremy.vangansberg@gmail.com>
2022-12-08 15:05:56 +0100

    changing port

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ def predict(payload:TextIn):
     return {'category': category_pred}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
